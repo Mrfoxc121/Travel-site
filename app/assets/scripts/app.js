d61165d2e2280a55f0a65347e292f958c1622728
Chris <foxc121@yahoo.co.uk>
2021-08-05 15:05:59 +0100

    complete mobile menu

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,20 +1,9 @@
  import '../styles/styles.css'
- import Person from './modules/Person'
+ import MobileMenu from './modules/MobileMenu';
+
+ let mobileMenu = new MobileMenu();
+
 
  if (module.hot) {
      module.hot.accept()
  }
-
- //  lesson example code below this line
-
- class Adult extends Person {
-     payTaxes() {
-         console.log(`${this.name} now owes zero taxes.`)
-     }
- }
-
- let John = new Person('John Doe', 'red');
- John.greet();
- let Jane = new Adult('Jane Smith', 'yellow');
- Jane.greet();
- Jane.payTaxes();
