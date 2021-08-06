21b4ece387e59b87c6053e84287c8ee27804cb7a
Chris <foxc121@yahoo.co.uk>
2021-08-06 11:16:19 +0100

    completed reveal section

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,10 @@
  import '../styles/styles.css'
  import MobileMenu from './modules/MobileMenu';
- import RevealOnScroll from './modules/RevealOnScroll';
+ import RevealOnScroll from './modules/RevealOnScroll'
 
- let revealOnScroll = RevealOnScroll();
- let mobileMenu = new MobileMenu();
+ new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
+ new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
+ let mobileMenu = new MobileMenu()
 
 
  if (module.hot) {
